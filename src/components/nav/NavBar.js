88c54a52982742815e3a66e3e6fda5d38860a5f2
Lauren Riddle <lari7132@gmail.com>
2020-02-17 10:27:19 -0600

    added mobile nav and resized cards

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,32 +6,32 @@ import { Navbar, Nav } from "react-bootstrap"
 
 
 const NavBar = () => (
-      <header>
-        <div className="flexContainer">
-          <img src={logo} alt="Cohort 36 Logo" id="classLogo"></img>
-          <div className="flexContainer2">
-            <h1 className="site-title flex-item">Cohort 36<br />
-              <small>Nashville Software School</small>
-            </h1>
-            <nav className="flex-item" id="navbar">
-            <Navbar  expand="lg" className="nav-width">
+  <header>
+    <div className="flexContainer">
+      <div className="flexContainer2">
+        {/* <h1 className="site-title flex-item">Cohort 36<br />
+          <small>Nashville Software School</small>
+        </h1> */}
+        <nav className="flex-item" id="navbar">
+          <Navbar expand="lg" className="nav-width">
 
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Collapse id="basic-navbar-nav">
               <ul className="container">
-                <li><Nav.Link className="nav-a" href="#home">Home</Nav.Link></li>
-                <li><Nav.Link className="nav-a" href="#about">About</Nav.Link></li>
-                <li><Nav.Link className="nav-a" href="#devs">Developers</Nav.Link></li>
-                <li><Nav.Link className="nav-a" href="#tech">Technologies</Nav.Link></li>
-                <li><Nav.Link className="nav-a" href="#thanks">Thanks</Nav.Link></li>
+                <Nav.Link className="nav-a" href="#home"><img src={logo} alt="Cohort 36 Logo" id="classLogo"></img></Nav.Link>
+                <Nav.Link className="nav-a" href="#home">Home</Nav.Link>
+                <Nav.Link className="nav-a" href="#about">About</Nav.Link>
+                <Nav.Link className="nav-a" href="#devs">Developers</Nav.Link>
+                <Nav.Link className="nav-a" href="#tech">Technologies</Nav.Link>
+                <Nav.Link className="nav-a" href="#thanks">Thanks</Nav.Link>
               </ul>
-              </Navbar.Collapse>
-              </Navbar>
-            </nav>
-            </div>
-        </div>
-      </header>
-    )
+            </Navbar.Collapse>
+          </Navbar>
+        </nav>
+      </div>
+    </div>
+  </header>
+)
 
 //use withRouter() when you can't use the Route component
 export default withRouter(NavBar);
