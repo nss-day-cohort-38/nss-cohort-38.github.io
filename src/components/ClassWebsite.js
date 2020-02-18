f661600c9bec2f48ba057ed92f4ddae39cde1ece
Lauren Riddle <lari7132@gmail.com>
2020-02-17 18:18:16 -0600

    added transparnet to solid nav

--- src/components/ClassWebsite.js
+++ src/components/ClassWebsite.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react'
 import './ClassWebsite.css'
 import NavBar from './nav/NavBar'
 import ApplicationViews from './ApplicationViews'
-
+import $ from "jquery"
 class ClassWebsite extends Component {
-
+  componentDidMount() {
+    if ($(window).scrollTop() > 150) {
+      $(".navbar-fixed-top").addClass("bg-nav");
+    } else {
+      $(".navbar-fixed-top").removeClass("bg-nav");
+    }
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 150) {
+        $(".navbar-fixed-top").addClass("bg-nav");
+      } else {
+        $(".navbar-fixed-top").removeClass("bg-nav");
+      }
+    });
+  }
   render() {
     return (
       <>
