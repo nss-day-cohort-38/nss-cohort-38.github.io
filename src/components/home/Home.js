7c23409bfc6aacc3c896d1cf2104e5d916f9d94e
Julian Garcia <garcia.j.julian@gmail.com>
2020-02-19 21:17:49 -0600

    added edited pitures and text commented out

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -46,8 +46,8 @@ class Home extends Component {
               alt="First slide"
             />
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              {/* <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
             </Carousel.Caption>
           </Carousel.Item>
 
@@ -59,8 +59,8 @@ class Home extends Component {
             />
 
             <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              {/* <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
